f5dba3cc7812de7bf3df7b14e9d008cf6354e480
chica002104 <b04303032@ntu.edu.tw>
2018-12-20 19:41:25 +0800

    Update PBC_FinalProject.py

--- PBC_FinalProject.py
+++ PBC_FinalProject.py
@@ -1,16 +1,2 @@
-<<<<<<< HEAD
 print ("HIIIIIIIIIIIIIIIIILU")
 print ("HIIIIIIIIIIIIIIIIILU2222222")
-=======
-
-for i in range(34):
-    print(i)
-
-
-for i in range(34):
-    print(i)
-
-
-for i in range(34):
-    print(i)
->>>>>>> Hung
